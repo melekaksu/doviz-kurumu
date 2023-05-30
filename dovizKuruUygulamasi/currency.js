235db283abaa12e1cea95d5c89fe235a58691f2f
Melek AKSU <melekkaksuu@gmail.com>
2023-05-30 18:23:42 +0300

    Döviz Kurumu

---
+++ dovizKuruUygulamasi/currency.js
@@ -0,0 +1,13 @@
+class Currency{
+    constructor(){
+        this.url = "https://api.freecurrencyapi.com/v1/latest?apikey=lusVgReTFOj30VUYhVAH8hsdRvyCsVSQ8uuEbNns&base_currency=";
+    }
+
+    async exchange(amount, firstCurrency, secondCurrency){
+        const response = await fetch(`${this.url}${firstCurrency}`);
+        const result = await response.json();
+        const exchangeResult = amount + result.data[secondCurrency];
+
+        return exchangeResult;
+    }
+}
